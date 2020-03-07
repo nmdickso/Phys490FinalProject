a565dfdf9d820217846f2f1c2e3e66be623c475f
Nolan Dickson <nmdickso@edu.uwaterloo.ca>
2020-03-07 13:12:58 -0500

    fix bug around array shapes

--- examples/copernicus/data_gen.py
+++ examples/copernicus/data_gen.py
@@ -9,14 +9,22 @@ a_e = 1.
 a_m = 1.52366231
 
 
-def generate_orbits(N_dataset, N_timeseries, del_t):
+def generate_orbits(N, M, del_t):
+    '''
+    N = N_dataset, amount of training datasets
+    M = N_timeseries, amount of time evolution iterations
+    del_t = amount of time (in days) for each time evolution iteration
+    '''
 
     # Generate initial mean anomalies
-    φ_e0 = (2.4 * np.pi) * np.random.rand(N_dataset) - (0.2 * np.pi)
-    φ_m0 = (2. * np.pi) * np.random.rand(N_dataset)
+    φ_e0 = (2.4 * np.pi) * np.random.rand(N) - (0.2 * np.pi)
+    φ_m0 = (2. * np.pi) * np.random.rand(N)
+
+    φ_e0 = φ_e0[:, np.newaxis].repeat(M, axis=1)
+    φ_m0 = φ_m0[:, np.newaxis].repeat(M, axis=1)
 
     # Compute Mean anomalies (φ)
-    time_series = np.arange(N_timeseries)
+    time_series = np.arange(M)[np.newaxis, :].repeat(N, axis=0)
 
     φ_e = φ_e0 + (2 * np.pi / T_e) * (time_series * del_t)
     φ_m = φ_m0 + (2 * np.pi / T_m) * (time_series * del_t)
@@ -32,3 +40,4 @@ def generate_orbits(N_dataset, N_timeseries, del_t):
     θ_e = φ_e
 
     return φ_e, φ_m, θ_e, θ_m
+
