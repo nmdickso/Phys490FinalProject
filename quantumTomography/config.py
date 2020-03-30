7ca7c3fa60d6ce2cb5730f1883ebc88ee9e3e325
PrinceOfPuppers <joshuamcpherson5@gmail.com>
2020-03-30 02:37:34 -0400

    Examples working, WIP optimization and refactoring

--- quantumTomography/config.py
+++ quantumTomography/config.py
@@ -1,4 +1,6 @@
 class Config:
     def __init__(self):
         self.dataPath="quantumTomography/data/"
+        self.pdfSavePath="quantumTomography/Results/"
         self.dataSetLen=100000
+        self.averaging=3
--- quantumTomography/main.py
+++ quantumTomography/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+import matplotlib.pyplot as plt
 import torch
 import torch.optim as optim
 from random import random
@@ -7,6 +8,8 @@ from config import Config
 from trainer import Trainer
 from scinet import Hyperparameters,Scinet
 from qubitGeneration import DataGen
+def createPdfLabel(numQubits):
+    return "{} _Qubit_Bar_Graph.pdf".format(numQubits)
 
 def createDataLabel(numQubits,tomComplete):
     if tomComplete:
@@ -31,6 +34,7 @@ def getDataArray(path,hyp,shuffle=True):
 class App:
     def __init__(self,hyp,cfg):
         #stores list of mse as a funciton of latent nodes, key is number of qubits
+        self.qbits=[1,2]
         self.QubitComp={1: [], 2: []}
         self.QubitIncomp={1: [], 2: []}
 
@@ -50,7 +54,7 @@ class App:
 
         self.hyp.testSize=1000
         self.hyp.trainBatchSize=512
-        self.hyp.epochs=50
+        self.hyp.epochs=40
         self.hyp.annealEpoch=self.hyp.epochs
 
         self.hyp.learningRate=1e-2
@@ -58,21 +62,8 @@ class App:
 
         self.hyp.latentNodes =latentNodes
 
-#generates dataset, creates network, trains network, returns MSE on test data
-    def getError(self,numQubits,latentNodes,tomComplete=True):
-        print(">>Finding Mean Square Error for {} Qubits and {} Latent Nodes".format(numQubits,latentNodes))
-        dimHilbert=2*(2**numQubits)-2
-        obsSize=self.observationDim[numQubits]
-        questionSize=self.questionDim[numQubits]
-
-        dataLabel=createDataLabel(numQubits,tomComplete)
-
-        dataGen=DataGen()
-
-        self.editHyp(obsSize,questionSize,latentNodes)
-        path=dataGen.generateDataSet(cfg,dataLabel,dimHilbert,obsSize,questionSize)
-        trainingData,testingData=getDataArray(path,self.hyp)
-
+    def getError(self,trainingData,testingData):
+        #network setup
         net=Scinet(hyp)
         if torch.cuda.is_available():
             net.device=torch.device("cuda:0")
@@ -82,20 +73,81 @@ class App:
 
         net.scheduler=optim.lr_scheduler.CosineAnnealingLR(net.optimizer,self.hyp.epochs,self.hyp.finalLr)
 
+        #training and testing for MSE
         trainer=Trainer(trainingData,testingData)
         trainer.train(net,hyp)
         error=trainer.getMSE(net)
+        return(error)
 
-        return error
+#generates dataset, creates network, trains network, returns MSE on test data
+#subspacedim is only relevent for tomographically incomplete data
+    def runQubitExample(self,numQubits,latentNodeList,tomComplete=True,subspaceDim=-1):
+        if tomComplete:
+            tomCompleteMessage="Tom. Complete"
+        else:
+            tomCompleteMessage="Tom. Incomplete"
 
+
+        dimHilbert=2*(2**numQubits)-2
+        
+        #observation binary projective measurement set is restricted to a subspace
+        #to generate tomographically incomplete data
+        if tomComplete:
+            obsSize=self.observationDim[numQubits]
+        else:
+            obsSize=subspaceDim
+        
+        questionSize=self.questionDim[numQubits]
+
+        errorList=len(latentNodeList)*[0]
+        
+        for i in range(0,self.cfg.averaging):
+            #data generation
+            dataLabel=createDataLabel(numQubits,tomComplete)
+            dataGen=DataGen()
+
+            path=dataGen.generateDataSet(cfg,dataLabel,dimHilbert,obsSize,questionSize)
+            trainingData,testingData=getDataArray(path,self.hyp)
+
+            for j,latentNodes in enumerate(latentNodeList):
+                print(">>Finding Mean Square Error for {} Qubits and {} Latent Nodes ({}), Averaging Run {}".format(numQubits,latentNodes,tomCompleteMessage,i))
+                self.editHyp(obsSize,questionSize,latentNodes)
+                errorList[j]+=self.getError(trainingData,testingData)
+            
+        errorList=[error/cfg.averaging for error in errorList]
+        return errorList
+    
+    def plotErrors(self):
+        for numQubits in self.qbits:
+            fig,ax=plt.subplots()
+            xs=np.arange(len(self.QubitComp[numQubits]))
+            barWidth=0.35
+            completeBars=ax.bar(xs-barWidth/2,self.QubitComp[numQubits],width=barWidth,color="blue",label="Tom. Complete")
+            IncompleteBars=ax.bar(xs+barWidth/2,self.QubitIncomp[numQubits],width=barWidth,color="orange",label="Tom. Incomplete")
+            ax.set_xlabel("Number of Latent Neurons")
+            ax.set_ylabel("Prediction Mean Square Error")
+            ax.set_title("One Qubit Example")
+            ax.set_xticks(xs)
+            ax.legend()
+            
+            plt.savefig(self.cfg.pdfSavePath+createPdfLabel(numQubits))
+            plt.show()
+    
     def main(self):
-        for numQubits in range(1,2):
-            for numLatent in range(0,6):
-                error=self.getError(numQubits,numLatent)
-                print('error:',error)
-                self.QubitComp[numQubits].append(error)
+        for numQubits in self.qbits:
+            latentList=range(0,2*(2**numQubits)+2)
 
-        print(self.QubitComp[1])
+            completeError=self.runQubitExample(numQubits,latentList)
+            self.QubitComp[numQubits]=completeError
+
+            incompleteError=self.runQubitExample(numQubits,latentList,False,2)
+            self.QubitIncomp[numQubits].append(incompleteError)
+
+        self.plotErrors()
+
+
+
+        
 
 
 
