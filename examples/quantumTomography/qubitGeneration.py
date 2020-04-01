71cfe7d0987365d153188ccb187d0776f8acd3be
Nolan Dickson <nmdickso@edu.uwaterloo.ca>
2020-04-01 16:12:45 -0400

    enforce pep-8 formatting in qubit example

--- examples/quantumTomography/qubitGeneration.py
+++ examples/quantumTomography/qubitGeneration.py
@@ -3,115 +3,121 @@ import os
 from random import uniform
 from config import Config
 
-def measureState(psi,phi):
-    return abs(np.inner(psi,phi))**2
+
+def measureState(psi, phi):
+    return abs(np.inner(psi, phi))**2
+
 
 def generateRandomQubit(dim):
-    qubit=np.zeros(dim,dtype=np.complex)
-    singular=True
+    qubit = np.zeros(dim, dtype=np.complex)
+    singular = True
     while singular:
-        for i in range(0,dim):
-            qubit[i]=uniform(0,1)+uniform(0,1)*1j
-        norm=np.linalg.norm(qubit)
-        singular=norm<0.0000001
-    
-    return qubit/norm
+        for i in range(0, dim):
+            qubit[i] = uniform(0, 1) + uniform(0, 1) * 1j
+        norm = np.linalg.norm(qubit)
+        singular = norm < 0.0000001
 
-def generateRandomSet(dim,numStates):
-    states=[]
-    for i in range(0,numStates):
+    return qubit / norm
+
+
+def generateRandomSet(dim, numStates):
+    states = []
+    for i in range(0, numStates):
         states.append(generateRandomQubit(dim))
-    states=np.array(states)
+    states = np.array(states)
     return(states)
 
-def parameterizeQuestion(omega,vecSet):
-    omega=[measureState(omega,vec) for vec in vecSet]
+
+def parameterizeQuestion(omega, vecSet):
+    omega = [measureState(omega, vec) for vec in vecSet]
     return omega
 
 
 class DataGen:
     def __init__(self):
-        self.psis=[]
-        self.basis=[]
-        self.observations=[]
-        self.questions=[]
-        self.answers=[]
+        self.psis = []
+        self.basis = []
+        self.observations = []
+        self.questions = []
+        self.answers = []
 
-        #used to parameterize omega
-        self.omegaBasis=[]
+        # used to parameterize omega
+        self.omegaBasis = []
 
-    def generateDataSet(self,cfg,dataSetLen,fileName,dimHilbert,obsSize,questionSize):
+    def generateDataSet(self, cfg, dataSetLen, fileName,
+                        dimHilbert, obsSize, questionSize):
         print("\n>>Generating New Dataset")
 
-        #generates psi
-        for i in range(0,dataSetLen):
-            phi=generateRandomQubit(dimHilbert)
+        # generates psi
+        for i in range(0, dataSetLen):
+            phi = generateRandomQubit(dimHilbert)
             self.psis.append(phi)
 
-        #generates phis (observationn basis)
-        self.basis=generateRandomSet(dimHilbert,obsSize)
+        # generates phis (observationn basis)
+        self.basis = generateRandomSet(dimHilbert, obsSize)
 
-        #generates observations
+        # generates observations
         for psi in self.psis:
-            obseravtion=[]
+            obseravtion = []
             for phi in self.basis:
-                measurement=measureState(psi,phi)
+                measurement = measureState(psi, phi)
                 obseravtion.append(measurement)
-            obseravtion=np.array(obseravtion)
+            obseravtion = np.array(obseravtion)
             self.observations.append(obseravtion)
-        
-        #generates omegas
-        for i in range(0,dataSetLen):
-            #generates omegas
+
+        # generates omegas
+        for i in range(0, dataSetLen):
+            # generates omegas
             self.questions.append(generateRandomQubit(dimHilbert))
 
-
-        #generates answers
-        for i in range(0,dataSetLen):
-            measurement=measureState(self.psis[i],self.questions[i])
+        # generates answers
+        for i in range(0, dataSetLen):
+            measurement = measureState(self.psis[i], self.questions[i])
             self.answers.append(np.array(measurement))
-        self.answers=np.array(self.answers)
+        self.answers = np.array(self.answers)
 
+        # maps omegas onto parameterized omegas (done after finding soltuions to
+        #   simplify math)
+        self.omegaBasis = generateRandomSet(dimHilbert, questionSize)
+        for i, omega in enumerate(self.questions):
+            self.questions[i] = parameterizeQuestion(omega, self.omegaBasis)
+        self.questions = np.array(self.questions, dtype=float)
 
-        #maps omegas onto parameterized omegas (done after finding soltuions to simplify math)
-        self.omegaBasis=generateRandomSet(dimHilbert,questionSize)
-        for i,omega in enumerate(self.questions):
-            self.questions[i]=parameterizeQuestion(omega,self.omegaBasis)
-        self.questions=np.array(self.questions,dtype=float)
-        
-        path = self.writeToFolder(cfg,dataSetLen,fileName)
+        path = self.writeToFolder(cfg, dataSetLen, fileName)
         return(path)
 
-    def writeToFolder(self,cfg,dataSetLen,label):
-        sets=[]
-        for i in range(0,dataSetLen):
-            dataSet=np.array((self.observations[i],self.questions[i],self.answers[i]))
+    def writeToFolder(self, cfg, dataSetLen, label):
+        sets = []
+        for i in range(0, dataSetLen):
+            dataSet = np.array(
+                (self.observations[i], self.questions[i], self.answers[i])
+            )
             sets.append(dataSet)
         np.array(sets)
 
-        #makes results folder if it does not exist
+        # makes results folder if it does not exist
         if not os.path.exists(cfg.dataPath[0:-1]):
             os.mkdir(cfg.dataPath[0:-1])
-        
-        i=1
+
+        i = 1
         while True:
-            path=cfg.dataPath+label+'_'+str(i)
-            i+=1
+            path = cfg.dataPath + label + '_' + str(i)
+            i += 1
             try:
-                f=open(path+".npy")
+                open(path + ".npy")
             except:
-                np.save(path,sets)
+                np.save(path, sets)
                 break
-            
-            if i==100:
+
+            if i == 100:
                 print("Unable to Save Data")
                 break
-    
+
         print("Data Written to {}".format(path))
-        return(path+'.npy')
+        return(path + '.npy')
+
 
 if __name__ == "__main__":
-    gen=DataGen()
-    cfg=Config()
+    gen = DataGen()
+    cfg = Config()
     gen.generateDataSet(cfg)
-    
--- examples/quantumTomography/trainer.py
+++ examples/quantumTomography/trainer.py
@@ -1,31 +1,43 @@
 import numpy as np
 from tqdm import tqdm
 import torch
-import torch.optim as optim
-import torch.nn as nn
 import torch.nn.functional as funct
 
+
 class Trainer:
-    def __init__(self,trainingData,testingData):
-        self.trainingLoss=[]
+    def __init__(self, trainingData, testingData):
+        self.trainingLoss = []
 
-        self.testingLoss=[]
-        
-        self.trainingObs=torch.from_numpy(np.array([row[0] for row in trainingData])).float()
-        self.trainQuestions=torch.from_numpy(np.array([row[1] for row in trainingData])).float()
-        self.trainingAns=torch.from_numpy(np.array([[row[2]] for row in trainingData])).float()
+        self.testingLoss = []
 
-        self.testingObs=torch.from_numpy(np.array([row[0] for row in testingData])).float()
-        self.testQuestions=torch.from_numpy(np.array([row[1] for row in testingData])).float()
-        self.testingAns=torch.from_numpy(np.array([[row[2]] for row in testingData])).float()
+        self.trainingObs = torch.from_numpy(np.array(
+            [row[0] for row in trainingData]
+        )).float()
+        self.trainQuestions = torch.from_numpy(np.array(
+            [row[1] for row in trainingData]
+        )).float()
+        self.trainingAns = torch.from_numpy(np.array(
+            [[row[2]] for row in trainingData]
+        )).float()
 
-    def train(self,neuralNet,hyp):
-        for i in tqdm(range(0,hyp.epochs)):
-            trainLoss=neuralNet.train(self.trainingObs,self.trainQuestions,self.trainingAns,hyp.trainBatchSize)
+        self.testingObs = torch.from_numpy(np.array(
+            [row[0] for row in testingData]
+        )).float()
+        self.testQuestions = torch.from_numpy(np.array(
+            [row[1] for row in testingData]
+        )).float()
+        self.testingAns = torch.from_numpy(np.array(
+            [[row[2]] for row in testingData]
+        )).float()
+
+    def train(self, neuralNet, hyp):
+        for i in tqdm(range(0, hyp.epochs)):
+            trainLoss = neuralNet.train(self.trainingObs, self.trainQuestions,
+                                        self.trainingAns, hyp.trainBatchSize)
             self.trainingLoss.append(trainLoss)
             neuralNet.scheduler.step()
 
-    def getMSE(self,net):
+    def getMSE(self, net):
         with torch.no_grad():
             observations = self.testingObs.to(net.device)
             answers = self.testingAns.to(net.device)
@@ -34,6 +46,5 @@ class Trainer:
             mu, sig, latent_activation, outputs = net(observations, questions)
 
             reconMSE = funct.mse_loss(outputs, answers).item()
-
 
         return reconMSE
